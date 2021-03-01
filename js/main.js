451d6c37d2c1f5350236d3d6738cb2bfdc3464f3
AlexDevRu <kulakov-alex01@mail.ru>
2021-03-01 21:28:51 +0300

    feat: add projects section

--- js/main.js
+++ js/main.js
@@ -2,3 +2,38 @@ var links  = document.querySelectorAll('.header__menu a');
 Array.from(links).forEach(function(link) {
 
 });
+
+var body = document.querySelector('body');
+var popup = document.getElementById('popup');
+var projects = document.getElementsByClassName("projects-item");
+var close = document.getElementsByClassName("close")[0];
+
+Array.from(projects).forEach(function(p) {
+	p.onclick = function() {
+		console.log(p);
+		console.log(popup);
+		popup.querySelector('.popup-image').append(p.querySelector('img').cloneNode(true));
+		popup.querySelector('.popup-text').append(p.querySelector('h3').cloneNode(true));
+		popup.querySelector('.popup-text').append(p.querySelector('.hidden-info').cloneNode(true));
+	    popup.style.display = "block";
+	    body.classList.toggle("lock");
+	};
+});
+
+close.onclick = function() {
+    popup.style.display = "none";
+    clear();
+};
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+        clear();
+    }
+};
+
+function clear() {
+	popup.querySelector('.popup-image').innerHTML = '';
+	popup.querySelector('.popup-text').innerHTML = '';
+	body.classList.toggle("lock");
+}
